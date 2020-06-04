8d95c14949af778754c6f8a444196c647bcdeec3
zuanwen <zuanwen.ang@snappymob.com>
2020-06-04 18:02:22 +0800

    Update main new fragment to use main new view model

--- app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/main_new/MainNewFragment.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/main_new/MainNewFragment.kt
@@ -1,6 +1,29 @@
 package com.alexchan.wallpaper.ui.wallpaper.main_new
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.alexchan.wallpaper.R
+import androidx.lifecycle.ViewModelProvider
+import com.alexchan.wallpaper.databinding.FragmentMainNewBinding
 
-class MainNewFragment : Fragment(R.layout.fragment_main_new)
+class MainNewFragment : Fragment() {
+
+    private val viewModel: MainNewViewModel by lazy {
+        ViewModelProvider(this).get(MainNewViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentMainNewBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the MainNewViewModel
+        binding.mainNewViewModel = viewModel
+
+        return binding.root
+    }
+}
