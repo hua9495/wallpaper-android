a5ecd6abcf2158b2875a60d8c317e9c238545115
zuanwen <zuanwen.ang@snappymob.com>
2020-06-09 19:37:00 +0800

    Add api request to get photo collections

--- app/src/main/java/com/alexchan/wallpaper/service/web/UnsplashApiService.kt
+++ app/src/main/java/com/alexchan/wallpaper/service/web/UnsplashApiService.kt
@@ -1,5 +1,6 @@
 package com.alexchan.wallpaper.service.web
 
+import com.alexchan.wallpaper.model.unsplash.Collection
 import com.alexchan.wallpaper.model.unsplash.Photo
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
@@ -11,6 +12,7 @@ import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 private const val ACCESS_KEY = "3O-0lx3tK8EB-5FUQnneiHneMwC0waml_N4jrNvv2pQ"
@@ -43,6 +45,14 @@ interface UnsplashApiService {
     @GET("photos")
     fun getPhotos():
             Deferred<List<Photo>>
+
+    @GET("users/{username}/collections")
+    fun getCollections(@Path("username") type: String):
+            Deferred<ArrayList<Collection>>
+
+    @GET("collections/{id}")
+    fun getCollectionsPhoto(@Path("id") type: Int):
+            Deferred<Collection>
 }
 
 // Singleton to expensive to call - so call once
