202bbf5c7d7dc245618b7ed59a65ea9ed4842b70
zuanwen <zuanwen.ang@snappymob.com>
2020-06-12 23:37:52 +0800

    Add wallpaper details fragment

---
+++ app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/dashboard/details/WallpaperDetailsFragment.kt
@@ -0,0 +1,47 @@
+package com.alexchan.wallpaper.ui.wallpaper.dashboard.details
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.alexchan.wallpaper.adapter.wallpaper.PhotoDetailsGridAdapter
+import com.alexchan.wallpaper.databinding.FragmentWallpaperDetailsBinding
+import com.alexchan.wallpaper.util.TAG
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_wallpaper_details.*
+
+class WallpaperDetailsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val application = requireNotNull(activity).application
+        val binding = FragmentWallpaperDetailsBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        val userPhotoCollection = WallpaperDetailsFragmentArgs.fromBundle(requireArguments()).selectedPhoto
+        val viewModelFactory = WallpaperDetailsViewModelFactory(userPhotoCollection, application)
+        val wallpaperDetailsViewModel = ViewModelProvider(this, viewModelFactory).get(WallpaperDetailsViewModel::class.java)
+        binding.wallpaperDetailsViewModel = wallpaperDetailsViewModel
+        binding.photoDetailsGrid.adapter = PhotoDetailsGridAdapter()
+
+        // Hide Top Toolbar
+        requireActivity().topToolbar.visibility = View.GONE
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set TitleTopToolbar OnClickListener for Wallpaper Details Fragment
+        requireActivity().titleTopToolbar.setOnClickListener { backToDashboardFragment() }
+    }
+
+    private fun backToDashboardFragment() {
+        Log.d(TAG, "Navigate back to previous fragment")
+        activity?.onBackPressed()
+    }
+}
