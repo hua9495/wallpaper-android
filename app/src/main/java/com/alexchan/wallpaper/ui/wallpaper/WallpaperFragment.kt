c11379519795aa05891f8fd5d4c2e556c64bf123
zuanwen <zuanwen.ang@snappymob.com>
2020-06-16 19:58:44 +0800

    Update view pager to view pager 2 implementation

--- app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/WallpaperFragment.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/WallpaperFragment.kt
@@ -3,47 +3,65 @@ package com.alexchan.wallpaper.ui.wallpaper
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.alexchan.wallpaper.R
 import com.alexchan.wallpaper.ui.wallpaper.dashboard.DashboardFragment
 import com.alexchan.wallpaper.ui.wallpaper.follow.FollowFragment
 import com.alexchan.wallpaper.ui.wallpaper.home.HomeFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_wallpaper.*
 
 class WallpaperFragment : Fragment(R.layout.fragment_wallpaper) {
 
-    private lateinit var adapter: WallpaperPagerAdapter
+    private lateinit var viewPagerAdapter: WallpaperPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = WallpaperPagerAdapter()
+        viewPagerAdapter = WallpaperPagerAdapter(this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewPager.adapter = adapter
-        viewPager.currentItem = 1
-        viewPager.offscreenPageLimit = 3
-        tabLayout.setupWithViewPager(viewPager)
+        setupViewPager()
     }
 
-    inner class WallpaperPagerAdapter :
-        FragmentStatePagerAdapter(childFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-
-        private val fragments = arrayOf(HomeFragment(), DashboardFragment(), FollowFragment())
-
-        override fun getItem(position: Int): Fragment {
-            return fragments[position]
+    private fun setupViewPager() {
+        viewPager.apply {
+            adapter = viewPagerAdapter
+            offscreenPageLimit = WallpaperPagerAdapter.FRAGMENT_OFF_SCREEN_PAGE_LIMIT as Int
+            setCurrentItem(1, false)
         }
 
-        override fun getCount(): Int = fragments.size
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return when (position) {
-                0 -> getString(R.string.home)
-                1 -> getString(R.string.dashboard)
+        TabLayoutMediator(tabLayout, viewPager) { currentTab, currentPosition ->
+            currentTab.text = when (currentPosition) {
+                WallpaperPagerAdapter.HOME_FRAGMENT_SCREEN_POSITION -> getString(R.string.home)
+                WallpaperPagerAdapter.DASHBOARD_FRAGMENT_SCREEN_POSITION -> getString(R.string.dashboard)
                 else -> getString(R.string.follow)
             }
+        }.attach()
+    }
+
+    internal class WallpaperPagerAdapter(wallpaperFragment: WallpaperFragment) :
+        FragmentStateAdapter(wallpaperFragment) {
+
+        override fun createFragment(position: Int): Fragment = when (position) {
+            HOME_FRAGMENT_SCREEN_POSITION -> HomeFragment()
+            DASHBOARD_FRAGMENT_SCREEN_POSITION -> DashboardFragment()
+            FOLLOW_FRAGMENT_SCREEN_POSITION -> FollowFragment()
+            else -> throw IllegalStateException("Invalid adapter position")
+        }
+
+
+        override fun getItemCount(): Int = TOTAL_FRAGMENT_SCREEN
+
+        companion object {
+            internal const val FRAGMENT_OFF_SCREEN_PAGE_LIMIT = 3
+
+            internal const val TOTAL_FRAGMENT_SCREEN = 3
+
+            internal const val HOME_FRAGMENT_SCREEN_POSITION = 0
+            internal const val DASHBOARD_FRAGMENT_SCREEN_POSITION = 1
+            internal const val FOLLOW_FRAGMENT_SCREEN_POSITION = 2
         }
     }
 }
