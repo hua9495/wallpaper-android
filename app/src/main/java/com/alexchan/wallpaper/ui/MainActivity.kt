6ed488a17c9fe17eeebd8640e398b26b79427b25
zuanwen <zuanwen.ang@snappymob.com>
2020-07-01 18:15:43 +0800

    Remove and update onMenuItemClickListener to dashboard fragment

--- app/src/main/java/com/alexchan/wallpaper/ui/MainActivity.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/MainActivity.kt
@@ -2,18 +2,13 @@ package com.alexchan.wallpaper.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.alexchan.wallpaper.R
 import com.alexchan.wallpaper.util.TAG
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,18 +16,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // Hold a Reference to PhotosGrid Recylerview
-    val photosGridRecylerView by lazy { findViewById<RecyclerView>(R.id.photosGrid) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Set TopToolbar NavigationOnClickListener
         topToolbar.setNavigationOnClickListener{openNavDrawer()}
-
-        // Set TopToolbar OnMenuItemClickListener
-        topToolbar.setOnMenuItemClickListener{item: MenuItem? -> onMenuItemClick(item)}
 
         // Update On Swipe Open Navigation Drawer Set Checked Item State
         updateOnSwipeOpenNavDrawer()
@@ -46,75 +35,6 @@ class MainActivity : AppCompatActivity() {
         if (navController != null) {
             NavigationUI.setupWithNavController(bottomNavigation, navController)
             NavigationUI.setupWithNavController(navigationView, navController)
-        }
-    }
-
-    private fun onMenuItemClick(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.search -> {
-
-                // Handle Search View
-                val searchView = item.actionView as SearchView
-                searchView.queryHint = getString(R.string.search_photos)
-                searchView.isSubmitButtonEnabled = true
-
-                // Hide and UnHide changeDisplayView Menu Item
-                searchView.setOnQueryTextFocusChangeListener { v, hasFocus ->
-                    topToolbar.menu.findItem(R.id.changeDisplayView).isVisible = !hasFocus
-                }
-
-                // Handle Search Query
-                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String): Boolean {
-                        searchView.clearFocus()
-
-                        if (!query.isNullOrEmpty()) {
-                            searchQuery = query
-                            searchStatus = true
-
-                            Toast.makeText(this@MainActivity, "Searching: $query", Toast.LENGTH_LONG).show()
-                            val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
-                            val navController = navHostFragment?.findNavController()
-                            navController?.navigate(R.id.wallpaperFragment)
-                        }
-                        return true
-                    }
-
-                    override fun onQueryTextChange(newText: String?): Boolean {
-                        if (!newText.isNullOrEmpty()) {
-                            Toast.makeText(this@MainActivity, "Searching: $newText", Toast.LENGTH_SHORT).show()
-
-                            searchQuery = newText
-                            searchStatus = true
-
-                            val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
-                            val navController = navHostFragment?.findNavController()
-                            navController?.navigate(R.id.wallpaperFragment)
-                        }
-                        return true
-                    }
-                })
-                true
-            }
-            R.id.listView -> {
-                // Handle List View
-                //photosGrid.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-                photosGridRecylerView.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
-                true
-            }
-            R.id.gridView -> {
-                // Handle Grid View
-                //unsplashPhotoImageView.scaleType = ImageView.ScaleType.CENTER_CROP
-                //unsplashPhotoImageView.requestLayout()
-                photosGridRecylerView.layoutManager = GridLayoutManager(this, 2)
-                true
-            }
-            R.id.staggeredGridView -> {
-                // Handle Staggered View
-                photosGridRecylerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-                true
-            }
-            else -> false
         }
     }
 
@@ -154,7 +74,14 @@ class MainActivity : AppCompatActivity() {
                 drawerLayout.closeDrawer(GravityCompat.START)
             }
             false -> when (!searchView.isIconified) {
-                true -> topToolbar.collapseActionView()
+                true -> {
+                    topToolbar.collapseActionView()
+                    searchQuery = ""
+                    searchStatus = false
+                    val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
+                    val navController = navHostFragment?.findNavController()
+                    navController?.navigate(R.id.wallpaperFragment)
+                }
                 false -> super.onBackPressed()
             }
         }
--- app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/dashboard/DashboardFragment.kt
@@ -2,18 +2,29 @@ package com.alexchan.wallpaper.ui.wallpaper.dashboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.alexchan.wallpaper.R
 import com.alexchan.wallpaper.adapter.wallpaper.PhotoGridAdapter
 import com.alexchan.wallpaper.databinding.FragmentDashboardBinding
+import com.alexchan.wallpaper.ui.MainActivity
 import com.alexchan.wallpaper.ui.wallpaper.WallpaperFragmentDirections
 import kotlinx.android.synthetic.main.activity_main.*
 
 class DashboardFragment : Fragment() {
+
+    // Hold a Reference to PhotosGrid Recylerview
+    val photosGridRecylerView by lazy { requireActivity().findViewById<RecyclerView>(R.id.photosGrid) }
 
     private val dashboardViewModel: DashboardViewModel by lazy {
         ViewModelProvider(this).get(DashboardViewModel::class.java)
@@ -43,6 +54,79 @@ class DashboardFragment : Fragment() {
         // Show Top Toolbar
         requireActivity().topToolbar.visibility = View.VISIBLE
 
+        // Set TopToolbar OnMenuItemClickListener
+        requireActivity().topToolbar.setOnMenuItemClickListener{item: MenuItem? -> onMenuItemClick(item)}
+
+
         return binding.root
+    }
+
+    private fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.search -> {
+
+                // Handle Search View
+                val searchView = item.actionView as SearchView
+                searchView.queryHint = getString(R.string.search_photos)
+                searchView.isSubmitButtonEnabled = true
+
+                // Hide and UnHide changeDisplayView Menu Item
+                searchView.setOnQueryTextFocusChangeListener { v, hasFocus ->
+                    requireActivity().topToolbar.menu.findItem(R.id.changeDisplayView).isVisible = !hasFocus
+                }
+
+                // Handle Search Query
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        searchView.clearFocus()
+
+                        if (!query.isNullOrEmpty()) {
+                            MainActivity.searchQuery = query
+                            MainActivity.searchStatus = true
+
+                            Toast.makeText(activity, "Searching: $query", Toast.LENGTH_LONG).show()
+                            val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
+                            val navController = navHostFragment?.findNavController()
+                            navController?.navigate(R.id.wallpaperFragment)
+                        }
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        if (!newText.isNullOrEmpty()) {
+                            Toast.makeText(activity, "Searching: $newText", Toast.LENGTH_SHORT).show()
+
+                            MainActivity.searchQuery = newText
+                            MainActivity.searchStatus = true
+
+                            val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
+                            val navController = navHostFragment?.findNavController()
+                            navController?.navigate(R.id.wallpaperFragment)
+                        }
+                        return true
+                    }
+                })
+                true
+            }
+            R.id.listView -> {
+                // Handle List View
+                //photosGrid.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                photosGridRecylerView.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+                true
+            }
+            R.id.gridView -> {
+                // Handle Grid View
+                //unsplashPhotoImageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                //unsplashPhotoImageView.requestLayout()
+                photosGridRecylerView.layoutManager = GridLayoutManager(requireContext(), 2)
+                true
+            }
+            R.id.staggeredGridView -> {
+                // Handle Staggered View
+                photosGridRecylerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                true
+            }
+            else -> false
+        }
     }
 }
