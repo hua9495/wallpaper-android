732296a0b95efda9c92f665cb143b38b2745e263
zuanwen <zuanwen.ang@snappymob.com>
2020-07-02 18:56:52 +0800

    Implement recylerview state restoration policy

--- app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/wallpaper/dashboard/DashboardFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -58,6 +59,14 @@ class DashboardFragment : Fragment() {
         requireActivity().topToolbar.setOnMenuItemClickListener{item: MenuItem? -> onMenuItemClick(item)}
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = DataBindingUtil.bind<FragmentDashboardBinding>(view)
+
+        // Experimental
+        binding?.photosGrid?.adapter?.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
     }
 
     private fun onMenuItemClick(item: MenuItem?): Boolean {
