ff1581624f3f91de7b99db557ea055348f164470
zuanwen <zuanwen.ang@snappymob.com>
2020-07-08 15:50:25 +0800

    Add confirmation dialog to clear recent search history

--- app/src/main/java/com/alexchan/wallpaper/ui/search/SearchActivity.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/search/SearchActivity.kt
@@ -3,18 +3,59 @@ package com.alexchan.wallpaper.ui.search
 import android.app.SearchManager
 import android.content.ComponentName
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.provider.SearchRecentSuggestions
 import android.view.Menu
 import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import com.alexchan.wallpaper.R
+import com.alexchan.wallpaper.database.SearchSuggestionProvider
 import com.alexchan.wallpaper.ui.search.searchResults.SearchResultsActivity
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // Set action bar background color
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(applicationContext, R.color.colorPrimary)))
+
+        // Allow the user to clear recent search history
+        clearSearchHistoryButton.setOnClickListener {clearSearchHistoryConfirmation()}
+    }
+
+    private fun clearSearchHistoryConfirmation() {
+        // Close software keyboard pop up
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(getString(R.string.confirmation_clear_search_history))
+            .setCancelable(false)
+            .setPositiveButton(getString(R.string.confirmation_clear_search_history_yes)) { _, _ ->
+                // Clear user recent search history
+                SearchRecentSuggestions(this, SearchSuggestionProvider.AUTHORITY, SearchSuggestionProvider.MODE)
+                    .clearHistory()
+                imm?.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+            }
+            .setNegativeButton(getString(R.string.confirmation_clear_search_history_no)) { dialog, _ ->
+                dialog.dismiss()
+                imm?.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+            }
+
+        val alert = builder.create()
+        alert.setCanceledOnTouchOutside(true)
+        alert.setOnCancelListener { dialog ->
+            dialog.dismiss()
+            imm?.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+        }
+        alert.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
