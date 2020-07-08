b4f9d77a7cd1a3e4098760057b340183bd72286b
zuanwen <zuanwen.ang@snappymob.com>
2020-07-08 08:23:41 +0800

    Add new SearchResultsActivity.kt

---
+++ app/src/main/java/com/alexchan/wallpaper/ui/search/searchResults/SearchResultsActivity.kt
@@ -0,0 +1,48 @@
+package com.alexchan.wallpaper.ui.search.searchResults
+
+import android.app.SearchManager
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.alexchan.wallpaper.R
+import com.alexchan.wallpaper.ui.MainActivity.Companion.searchQuery
+import com.alexchan.wallpaper.ui.MainActivity.Companion.searchStatus
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SearchResultsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set Search Wallpaper Navigation Host
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
+        val navController = navHostFragment?.findNavController()
+        navController?.setGraph(R.navigation.search_wallpaper)
+
+        // Hide Bottom Navigation
+        bottomNavigation.visibility = View.GONE
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+
+            // Use the query to search data
+            if (!query.isNullOrEmpty()) {
+                Toast.makeText(this, "Searching: $query", Toast.LENGTH_LONG).show()
+                searchQuery = query
+                searchStatus = true
+            }
+        }
+    }
+}
