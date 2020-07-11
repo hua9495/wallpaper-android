182d85c70538c9c6c977a40dc84dd79acaafa319
zuanwen <zuanwen.ang@snappymob.com>
2020-07-11 19:00:54 +0800

    Add network error lottie animation view

--- app/src/main/java/com/alexchan/wallpaper/adapter/wallpaper/PhotoStatusBindingAdapter.kt
+++ app/src/main/java/com/alexchan/wallpaper/adapter/wallpaper/PhotoStatusBindingAdapter.kt
@@ -3,6 +3,7 @@ package com.alexchan.wallpaper.adapter.wallpaper
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.airbnb.lottie.LottieAnimationView
 import com.alexchan.wallpaper.R
 import com.alexchan.wallpaper.ui.wallpaper.dashboard.UnsplashApiStatus
 
@@ -14,11 +15,27 @@ fun bindStatus(statusImageView: ImageView, status: UnsplashApiStatus?) {
             statusImageView.setImageResource(R.drawable.loading_animation_api_status)
         }
         UnsplashApiStatus.ERROR -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.ic_baseline_cloud_off)
+            //statusImageView.visibility = View.VISIBLE
+            //statusImageView.setImageResource(R.drawable.ic_baseline_cloud_off)
+            statusImageView.visibility = View.GONE
         }
         UnsplashApiStatus.DONE -> {
             statusImageView.visibility = View.GONE
+        }
+    }
+}
+
+@BindingAdapter("unsplashApiStatusWithLottie")
+fun bindStatusWithLottie(statusLottieAnimationView: LottieAnimationView, status: UnsplashApiStatus?) {
+    when (status) {
+        UnsplashApiStatus.LOADING -> {
+            statusLottieAnimationView.visibility = View.GONE
+        }
+        UnsplashApiStatus.ERROR -> {
+            statusLottieAnimationView.visibility = View.VISIBLE
+        }
+        UnsplashApiStatus.DONE -> {
+            statusLottieAnimationView.visibility = View.GONE
         }
     }
 }
