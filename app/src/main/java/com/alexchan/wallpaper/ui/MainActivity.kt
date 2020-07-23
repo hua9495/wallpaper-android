3ae8940cdc712b01d1b0d64844fcb5038ceb5b5d
zuanwen <zuanwen.ang@snappymob.com>
2020-07-23 16:12:53 +0800

    Remove navigation drawer implementation

--- app/src/main/java/com/alexchan/wallpaper/ui/MainActivity.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/MainActivity.kt
@@ -2,10 +2,7 @@ package com.alexchan.wallpaper.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.alexchan.wallpaper.R
@@ -19,15 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Set TopToolbar NavigationOnClickListener
-        //topToolbar.setNavigationOnClickListener{openNavDrawer()}
-
-        // To disable on swipe open navigation drawer gesture
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, GravityCompat.START)
-
-        // Update On Swipe Open Navigation Drawer Set Checked Item State
-        updateOnSwipeOpenNavDrawer()
-
         // Default Wallpaper Navigation Host
         Log.d(TAG, "Wallpaper Navigation host created by default")
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
@@ -36,53 +24,6 @@ class MainActivity : AppCompatActivity() {
         // Set up Bottom Navigation and Navigation Drawer with navController
         if (navController != null) {
             NavigationUI.setupWithNavController(bottomNavigation, navController)
-            NavigationUI.setupWithNavController(navigationView, navController)
         }
-    }
-
-    // Open Navigation Drawer
-    private fun openNavDrawer() {
-        updateNavigationDrawerSetCheckedItem()
-        Log.d(TAG, "Drawer is Open")
-        drawerLayout.openDrawer(GravityCompat.START)
-    }
-
-    // Update on Swipe Open Navigation Drawer Set Checked Item State
-    private fun updateOnSwipeOpenNavDrawer() {
-        drawerLayout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
-            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-                super.onDrawerSlide(drawerView, slideOffset)
-                updateNavigationDrawerSetCheckedItem()
-            }
-        })
-    }
-
-    // Update Navigation Drawer Set Checked Item State
-    private fun updateNavigationDrawerSetCheckedItem() {
-        Log.d(TAG, supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)
-            ?.findNavController()?.currentDestination?.id.toString())
-        when (supportFragmentManager.findFragmentById(R.id.mainNavHostFragment)?.findNavController()?.currentDestination?.id) {
-            R.id.wallpaperFragment -> navigationView.setCheckedItem(R.id.wallpaperFragment)
-            R.id.notificationFragment -> navigationView.setCheckedItem(R.id.notificationFragment)
-            R.id.profileFragment -> navigationView.setCheckedItem(R.id.profileFragment)
-        }
-    }
-
-    override fun onBackPressed() {
-        when (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            true -> {
-                Log.d(TAG, "Drawer is Closed")
-                drawerLayout.closeDrawer(GravityCompat.START)
-            }
-            false -> super.onBackPressed()
-        }
-    }
-
-    companion object {
-        var searchQuery : String = ""
-        var searchStatus : Boolean = false
-        var pageNumber : Int = 1
-        var paginationStatus : Boolean = false
-        var showPagination : Boolean = true
     }
 }
