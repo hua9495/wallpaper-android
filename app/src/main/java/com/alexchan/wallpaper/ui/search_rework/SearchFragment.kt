13ea1ef42c0f01adc8ce2c1110fddd42243bbf88
zuanwen <zuanwen.ang@snappymob.com>
2020-07-29 13:17:10 +0800

    Fix search navigation in SearchFragment.kt

--- app/src/main/java/com/alexchan/wallpaper/ui/search_rework/SearchFragment.kt
+++ app/src/main/java/com/alexchan/wallpaper/ui/search_rework/SearchFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.alexchan.wallpaper.R
+import com.alexchan.wallpaper.ui.search_rework.search_results.SearchResultsFragmentDirections
 import com.alexchan.wallpaper.ui.search_rework.search_suggestions.SearchSuggestionsFragmentDirections
 import kotlinx.android.synthetic.main.fragment_search.*
 
@@ -29,51 +31,92 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Set up search toolbar back navigation
         searchToolbar.setNavigationOnClickListener { backToDashboard() }
 
-        // Hide Bottom Navigation in Search Fragment
-        //requireActivity().bottomNavigation.isGone = true
+        // Set up search toolbar to request focus when launch
+        setupSearchToolbar()
 
-        searchTextInputEditText.requestFocus()
+        // Perform search based on the keyboard soft input search button clicked
+        searchTextInputEditText.setOnEditorActionListener { v, actionId, _ ->
+            onActionPerform(
+                v,
+                actionId
+            )
+        }
+
+        // Handle search query change
+        searchTextInputEditText.addTextChangedListener { searchQueryIsChangeAndShowSearchSuggestions() }
+
+        // Handle close button clicked
+        searchTextInputLayout.setEndIconOnClickListener { clearSearchQueryAndShowSearchSuggestions() }
+    }
+
+    private fun showSoftInputKeyboard() {
         val imm =
             requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         imm?.showSoftInput(searchTextInputEditText, InputMethodManager.SHOW_IMPLICIT)
+    }
 
-        searchTextInputEditText.setOnEditorActionListener { v, actionId, event ->
-            when (actionId) {
-                EditorInfo.IME_ACTION_SEARCH -> {
-                    // Handle search
+    private fun hideSoftInputKeyboard() {
+        val imm =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.hideSoftInputFromWindow(searchTextInputEditText.windowToken, 0)
+    }
+
+    private fun setupSearchToolbar() {
+        searchTextInputEditText.requestFocus()
+        showSoftInputKeyboard()
+    }
+
+    private fun onActionPerform(v: TextView, actionId: Int): Boolean {
+        return when (actionId) {
+            EditorInfo.IME_ACTION_SEARCH -> {
+                // Handle search
+                if (!v.text.isNullOrBlank()) {
                     v.clearFocus()
                     showSearchResults(v.text.toString())
-                    true
                 }
-                else -> false
+                true
             }
+            else -> false
         }
+    }
 
-        searchTextInputEditText.addTextChangedListener {
-            Navigation.findNavController(requireActivity(), R.id.searchFragmentContainerView)
-                .navigateUp()
+    private fun searchQueryIsChangeAndShowSearchSuggestions() {
+        if (!searchTextInputEditText.text.isNullOrBlank()) {
+            searchQueryIsChangeShowSearchSuggestions()
         }
+    }
 
-        searchTextInputLayout.setEndIconOnClickListener {
-            // Display Search Suggestions
-            if (!searchTextInputEditText.text.isNullOrBlank()) {
-                searchTextInputEditText.text?.clear()
-                val imm =
-                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-                imm?.showSoftInput(searchTextInputEditText, InputMethodManager.SHOW_IMPLICIT)
-            }
+    private fun clearSearchQueryAndShowSearchSuggestions() {
+        // Display Search Suggestions
+        if (!searchTextInputEditText.text.isNullOrBlank()) {
+            searchTextInputEditText.text?.clear()
+            showSoftInputKeyboard()
             searchTextInputEditText.requestFocus()
-            Navigation.findNavController(requireActivity(), R.id.searchFragmentContainerView)
-                .navigateUp()
+            searchQueryIsChangeShowSearchSuggestions()
+        }
+    }
+
+    private fun searchQueryIsChangeShowSearchSuggestions() {
+        if (Navigation.findNavController(
+                requireActivity(),
+                R.id.searchFragmentContainerView
+            ).currentDestination?.id != R.id.searchSuggestionsFragment
+        ) {
+            Navigation.findNavController(
+                requireActivity(),
+                R.id.searchFragmentContainerView
+            )
+                .navigate(
+                    SearchResultsFragmentDirections.actionShowSearchSuggestions()
+                )
         }
     }
 
     private fun showSearchResults(searchQuery: String) {
-        val imm =
-            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        imm?.hideSoftInputFromWindow(searchTextInputEditText.windowToken, 0)
+        hideSoftInputKeyboard()
         Toast.makeText(requireContext(), "Searching: $searchQuery", Toast.LENGTH_LONG).show()
 
         Navigation.findNavController(requireActivity(), R.id.searchFragmentContainerView)
@@ -81,9 +124,7 @@ class SearchFragment : Fragment() {
     }
 
     private fun backToDashboard() {
-        val imm =
-            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        imm?.hideSoftInputFromWindow(searchTextInputEditText.windowToken, 0)
+        hideSoftInputKeyboard()
         requireActivity().findNavController(R.id.mainNavHostFragment).navigateUp()
     }
 }
